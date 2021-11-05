99caf3451524943bf20812d3720ccea7eafbaf53
jeff <jmorton138@gmail.com>
2021-11-05 10:17:10 +0000

    Basic RESTful KittensController actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,51 @@
 class KittensController < ApplicationController
+
+    def index
+        @kittens = Kitten.all
+    end
+
+    def show
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def new
+        @kitten = Kitten.new
+    end
+
+    def create
+        @kitten = Kitten.new(kitten_params)
+
+        if @kitten.save
+            redirect_to kitten_path(@kitten.id)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def update
+        @kitten = Kitten.find(params[:id])
+
+        if @kitten.updat(kitten_params)
+            redirect_to kitten_path(@kitten.id)
+        else
+            render :edit
+        end
+
+    end
+
+    def destroy
+        @kitten = Kitten.find(params[:id])
+        @kitten.destroy
+
+        redirect_to root_path
+    end
+
+    private
+
+    def kitten_params
+        params.require(:kitten).permit(:name, :age, :cuteness, :softeness)
 end
